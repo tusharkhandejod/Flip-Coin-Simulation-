55bdc89b0a5bacf12e155303cb1153f729ac209b
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-04 12:17:03 +0530

    Flipping a coin multiple times and displaying head or tail counts

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 echo "Welcome to the Flip Coun Simulation program"
+read -p "Enter the no of times you wants coin to be flipped=" n
+headwincount=0
+tailwincount=0
+for (( i=1; i<=n; i++ ))
+do
 x=$(($RANDOM%2))
 if [[ $x -eq 0 ]]
 then
 echo "Head is winner"
+headwincount=$(($headwincount+1))
 elif [[ $x -eq 1 ]]
 then
 echo "Tail is winner"
+tailwincount=$(($tailwincount+1))
 fi
-
+done
+echo "Head win count=" $headwincount
+echo "Tail win count=" $tailwincount
