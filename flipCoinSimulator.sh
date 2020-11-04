1098318fc3b74e6f9f4d42a662d627d66bb2d872
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-04 12:30:12 +0530

    If tie occures then coin flip continues until difference of two or more than two occures

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,6 +3,7 @@ echo "Welcome to the Flip Coun Simulation program"
 headwincount=0
 tailwincount=0
 diff=0
+difference=0
 while [[ $headwincount -le 20 && $tailwincount -le 20 ]]
 do
 x=$(($RANDOM%2))
@@ -29,5 +30,21 @@ diff=$(($tailwincount-$headwincount))
 echo "Tail wins by $diff count"
 elif [[ $headwincount -eq 21 && $tailwincount -eq 21 ]]
 then
-echo "It is a Tie"
+echo "It is a Tie and the flipping of coin continues till difference of 2 or more than 2 is achieved."
+while [[ $difference -le 2 ]]  
+do
+x=$(($RANDOM%2))
+if [[ $x -eq 0 ]]
+then
+echo "Head wins"
+headwincount=$(($headwincount+1))
+$difference=$(($headwincount-$tailwincount))
+elif [[ $x -eq 1 ]]
+then
+echo "Tail is winner"
+tailwincount=$(($tailwincount+1))
+difference=$(($tailwincount-$headwincount))
 fi
+done
+fi
+
